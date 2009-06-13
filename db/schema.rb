dcbed5aef887f86b15526435370e2878a9a1ebba
Alice Zhang <ms.alice.zhang@gmail.com>
2009-06-13 14:16:16 -0700

    topic crud

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090613171918) do
+ActiveRecord::Schema.define(:version => 20090613210159) do
+
+  create_table "topics", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
